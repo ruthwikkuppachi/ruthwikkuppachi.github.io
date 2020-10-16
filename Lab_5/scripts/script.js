1ae0640579feaa8165ed6175573b76132a0edd28
ruthwik11 <51654907+ruthwik11@users.noreply.github.com>
2020-10-15 21:38:14 -0400

    Update script.js

--- Lab_5/scripts/script.js
+++ Lab_5/scripts/script.js
@@ -11,20 +11,21 @@ function getRandomInt25(min, max) {
         max = Math.floor(max);
         n = Math.floor(Math.random() * (max - min + 1) + min);
         arr.push(n);
-      }
+       
+    }
       return arr;
     }
 
   
     function arrToUl(root, arr) {
         var li;
-        root.appendChild(ol); // append the created ul to the root
-      
+        root.appendChild(ol);
         arr.forEach(function(country, index) {
-        if(index in getRandomInt25(0,240)){
-          li = document.createElement('li'); // create a new list item
-          li.appendChild(document.createTextNode(country.code + " " + country.name)); // append the text to the li
-          ol.appendChild(li); // append the list item to the ul
+        if(getRandomInt25(0,243).includes(index)){
+          li = document.createElement('li');
+          li.appendChild(document.createTextNode(country.code));
+          li.append(document.createTextNode(" " + country.name));
+          ol.appendChild(li);
         }
           
         });
