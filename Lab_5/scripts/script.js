78258fec1554fa49b24762dd5b3e84e849027883
ruthwik11 <51654907+ruthwik11@users.noreply.github.com>
2020-10-15 21:05:47 -0400

    Update script.js

--- Lab_5/scripts/script.js
+++ Lab_5/scripts/script.js
@@ -1,4 +1,33 @@
-let myContent = document.getElementsByClassName("content");
+let myContent = document.getElementsByClassName("content")[0];
 let ol = document.createElement("ol");
-myContent.appendChild(ol);
 ol.classList.add("countries");
+
+const btn = document.getElementsByClassName('activate');
+
+function getRandomInt25(min, max) {
+    var arr = [];
+    for (i=0; i<25; i++){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        n = Math.floor(Math.random() * (max - min + 1) + min);
+        arr.push(n);
+      }
+      return arr;
+    }
+
+  
+    function arrToUl(root, arr) {
+        var li;
+        root.appendChild(ol); // append the created ul to the root
+      
+        arr.forEach(function(country, index) {
+        if(index in getRandomInt25(0,240)){
+          li = document.createElement('li'); // create a new list item
+          li.appendChild(document.createTextNode(country.code + " " + country.name)); // append the text to the li
+          ol.appendChild(li); // append the list item to the ul
+        }
+          
+        });
+      }
+
+      btn.onclick = arrToUl(myContent, countries);
